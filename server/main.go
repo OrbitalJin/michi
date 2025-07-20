aa41335d4548e0d32f7c7561862ead0e53400624
orbitaljin <ouzaher.saad@yahoo.fr>
2025-07-20 07:19:01 +0000

    bang regex detection

--- server/main.go
+++ server/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"os"
+	"regexp"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -11,6 +13,23 @@ import (
 const DB = "./foo.db"
 
 func main() {
+
+	pattern := `!\b\w+\b`
+	target := "!t3 fdsfdslj !213 !go ! hello"
+	re, err := regexp.Compile(pattern)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	matches := re.FindAllString(target, -1)
+
+	if len(matches) > 0 {
+		for i, match := range matches {
+			fmt.Printf("match %d, %s \n", i, match)
+		}
+	}
+
 	os.Remove(DB)
 	db, err := sql.Open("sqlite3", DB)
 
