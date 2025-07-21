92785931b482bd1962458b840ae9c24936bb85f1
orbitaljin <ouzaher.saad@yahoo.fr>
2025-07-21 07:48:30 +0000

    server db CRUD operations & conn closing

--- server/internal/database/main.go
+++ server/internal/database/main.go
@@ -64,6 +64,64 @@ func (db *DB) GetBang(alias string) (*Bang, error) {
 	return &bang, nil
 }
 
+func (db *DB) CreateBang(alias, url string) (*Bang, error) {
+	bang := Bang{
+		Alias: alias,
+		URL:   url,
+	}
+
+	stmnt := "INSERT INTO bangs (alias, url) VALUES (?, ?)"
+
+	res, err := db.conn.Exec(
+		stmnt,
+		bang.Alias,
+		bang.URL,
+	)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute the bang creation statement: %s", err)
+	}
+
+	id, err := res.LastInsertId()
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to insert bang: %s", err)
+	}
+
+	bang.ID = int(id)
+
+	return &bang, nil
+}
+
+func (db *DB) GetBangs() (*[]Bang, error) {
+	var bangs []Bang
+
+	stmnt := "SELECT id, alias, url FROM bangs"
+
+	rows, err := db.conn.Query(stmnt)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to query bangs: %s", err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var bang Bang
+
+		if err := rows.Scan(&bang.ID, &bang.Alias, &bang.URL); err != nil {
+			return nil, fmt.Errorf("failed to scan row: %s", err)
+		}
+		bangs = append(bangs, bang)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("error occured while stepping through the rows: %s", err)
+	}
+
+	return &bangs, nil
+}
+
 func (db *DB) Shutdown() {
 	db.conn.Close()
 }
